add9f8561c813a90b13ac0404c86caa69741f565
AliNisarAhmed <ali.nisar87@gmail.com>
2018-11-14 19:18:10 +0500

    end

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -10,20 +10,32 @@ app.use(urlencoded({extended: true}))
 app.use(json())
 
 app.get('/todo/:id', async (req, res) => {
-  const todoId = req.params.id;
-  const todo = await Todo.findById(todoId).lean().exec();
-  res.status(200).json(todo); 
-})
+  try {
+    const todoId = req.params.id;
+    const todo = await Todo.findById(todoId).lean().exec();
+    res.status(200).json(todo); 
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 app.get('/todos', async (req, res) => {
-  const todos = Todo.find({}).lean().exec();
-  res.status(200).json(todos);
+  try {
+    const todos = await Todo.find({}).lean().exec();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).send();
+  }
 })
 
 app.post('/todo', async () => {
-  const todoToCreate = req.body.todo;
-  const todo = Todo.create(todoToCreate)
-  res.status(201).json(todo.toJSON());
+  try {
+    const todoToCreate = req.body.todo;
+    const todo = await Todo.create(todoToCreate)
+    res.status(201).json(todo.toJSON());
+  } catch (error) {
+    res.status(500).send()
+  }
 })
 
 connect('mongodb://localhost:27017/myTodos')
